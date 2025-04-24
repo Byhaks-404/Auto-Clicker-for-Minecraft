ddcdebf4bab5e2c712104945a45c7a8692743d59
byhaks <byhaks@gmail.com>
2025-04-24 17:44:21 +0200

    commit

---
+++ auto_clicker2.py
@@ -0,0 +1,31 @@
+import pyautogui
+import time
+import random
+from pynput import mouse
+import keyboard
+
+clicking = False
+
+def on_click(x,y,button, pressed):
+    global clicking
+    if pressed:
+        if button == mouse.Button.x2:  
+            clicking = True
+        elif button == mouse.Button.x1:  
+            clicking = False
+              
+
+listener = mouse.Listener(on_click=on_click)
+listener.start()
+
+while True:
+    if clicking:
+        pyautogui.click()
+        random_number = random.uniform(0.001, 0.034)  
+        time.sleep(random_number)
+    
+    time.sleep(0.01)  
+    if keyboard.is_pressed('`'):
+        break
+
+listener.stop()
